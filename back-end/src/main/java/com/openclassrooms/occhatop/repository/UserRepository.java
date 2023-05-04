a9251604782c0c08a3bf89eb67f9e5cc7a9fbd2a
Stephane Hua <>
2023-05-05 00:03:52 +0200

    #1.2 Add User and UserRepository model and his test to our database

---
+++ back-end/src/main/java/com/openclassrooms/occhatop/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.openclassrooms.occhatop.repository;
+
+import com.openclassrooms.occhatop.model.auth.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
---
+++ back-end/src/test/java/com/openclassrooms/occhatop/repository/UserRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.occhatop.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.openclassrooms.occhatop.model.auth.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+
+    // test methods go below
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("test@gmail.com");
+        user.setPassword("test1234");
+        user.setName("Test");
+
+        User savedUser = repo.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+        assertThat(existUser.getCreatedAt()).isNotNull();
+    }
+}
